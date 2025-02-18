2df2dd15c81fa4d07027c20a20de1a5d2f651c9e
Pedro Afonso <137439531+PedroAfonso24@users.noreply.github.com>
2025-02-18 12:03:36 -0300

    Estudos na manhã 18_02

---
+++ Tudo Java Script/ex015/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 4, 6]
+
+
+// num[6] = 3 Coloca o número na posição em colchetes 
+
+num.length // Ele fala quantos elementos tem na array
+
+num.push(7) // Coloca o número em parenteres na última posição //
+
+num.sort() // Coloca todos os elementos em forma crescente
+
+console.log(`Nosso vetor é o ${num}`)
